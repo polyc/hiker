8189150b96ca84a5285387bcf346cf6824add795
regibald <martinelli.fulco@gmail.com>
2018-09-06 17:28:40 +0200

    created methods for login and logout in users_controller.rb user.rb application_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,28 @@
 class UsersController < ApplicationController
+
+  before_filter :save_login_state, :only => [:new, :create]
+
   def index
     @users = User.all.order(:nickname)
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:notice] = "Signup successfull"
+    ##  redirect to login page da implementare
+    else
+      flash[:notice] = "Form invalid"
+      flash[:color]  = "invalid"
+      render "new"
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :surname, :gender, :birthdate, :nickname, :email, :city, :description)
   end
 end
