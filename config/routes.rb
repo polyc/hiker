3c48165a4cec27ea98cb6e1928f1f1b6f82e4ec2
polyc <ichigo69600@gmail.com>
2018-09-06 21:59:44 +0200

    add basic session routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users
-	root :to => redirect('/users')
+	#root :to => redirect('/users')
+
+  root :to => redirect('/users')
+  get "signup", :to => "users#new"
+  get "login", :to => "sessions#login"
+  get "logout", :to => "sessions#logout"
+  get "home", :to => "sessions#home"
+  get "profile", :to => "sessions#profile"
+  get "setting", :to => "sessions#setting"
 end
