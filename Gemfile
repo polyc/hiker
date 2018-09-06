025c84b3c3713aae6bf23a63029a7950ce6a6ae9
polyc <ichigo69600@gmail.com>
2018-09-06 16:17:27 +0200

    add user model,view,controller and add dev db with initial migration

--- Gemfile
+++ Gemfile
@@ -20,6 +20,8 @@ gem 'uglifier', '>= 1.3.0'
 # gem 'therubyracer', platforms: :ruby
 # use Haml for templates
 gem 'haml'
+#Use Bcrypt for password encryption
+gem 'bcrypt-ruby', :require => 'bcrypt' 
 
 # Use CoffeeScript for .coffee assets and views
 gem 'coffee-rails', '~> 4.2'
---
+++ db/migrate/20180905145950_create_users.rb
@@ -0,0 +1,18 @@
+class CreateUsers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :surname
+      t.string :gender
+      t.datetime :birthdate
+      t.string :nickname
+      t.string :email
+      t.text :description
+      t.string :city
+      t.string :encrypted_password
+      t.string :salt
+
+      t.timestamps
+    end
+  end
+end
