b7bc41a3dba620130deadb681d851653446d29b4
polyc <ichigo69600@gmail.com>
2018-09-08 12:31:49 +0200

    fix a call to an User's attribute

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,7 +31,7 @@ class User < ActiveRecord::Base
     if  EMAIL_REGEX.match(username_or_email)
       user = User.find_by_email(username_or_email)
     else
-      user = User.find_by_username(username_or_email)
+      user = User.find_by_nickname(username_or_email)
     end
     if user && user.match_password(login_password)
       return user
