09a196a63fa1c72586f7d4a13617822cc4c4b608
polyc <ichigo69600@gmail.com>
2018-09-09 17:22:38 +0200

    adds routes for Comment CRUD and a test hike table

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,18 @@ Rails.application.routes.draw do
 
   root :to => redirect('/users')
   get "signup", :to => "users#new"
+
   get "login", :to => "sessions#login"
   get "logout", :to => "sessions#logout"
   get "home", :to => "sessions#home"
   get "profile", :to => "sessions#profile"
   get "setting", :to => "sessions#setting"
   post "login_attempt", :to => "sessions#login_attempt"
+
+  get "hikes", :to => "hikes#index"
+
+  #get 'comments/create,'
+  #get 'comments/update,'
+  #get 'comments/show,'
+  #get 'comments/delete'
 end
