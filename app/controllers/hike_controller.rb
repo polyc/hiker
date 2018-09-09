1ced0ade81ce2a734c00179cedc541fc6cb14597
regibald <martinelli.fulco@gmail.com>
2018-09-09 12:55:28 +0200

    created hikes_controller

--- app/controllers/hike_controller.rb
+++
@@ -1,2 +0,0 @@
-class HikeController < ApplicationController
-end
---
+++ app/controllers/hikes_controller.rb
@@ -0,0 +1,27 @@
+class HikesController < ApplicationController
+
+  def index
+    @hikes = Hike.all.order(:timestamps)
+  end
+
+  def new
+    @hike = Hike.new
+  end
+
+  def create
+    @hike = Hike.new(hike_params)
+      if @hike.save
+        flash[:notice]= "Hike created successfully"
+        #redirect_to hike_path
+      else
+        flash[:warning]= "Invalid form compilation"
+        render "new"
+      end
+  end
+
+  private
+  def hike_params
+    params.require(:hike).permit(:name, :address, :equipment, :difficulty, :distance, :nature, :max_height, :min_height, :rating, :type, :description)
+  end
+
+end
