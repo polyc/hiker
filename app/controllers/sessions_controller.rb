bd9ce320ece9eaf497c8b40e9a3ac5cccd0e9c8e
regibald <martinelli.fulco@gmail.com>
2018-09-18 11:34:34 +0200

    created search bar in navigation bar

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,5 @@
 class SessionsController < ApplicationController
-  before_action :authenticate_user, :only => [:home, :profile, :setting]
+  before_action :authenticate_user, :only => [:home, :profile, :setting, :search]
   before_action :save_login_state, :only => [:login, :login_attempt]
 
   def login
@@ -9,12 +9,21 @@ class SessionsController < ApplicationController
     authorized_user = User.authenticate(params[:username_or_email],params[:login_password])
     if authorized_user
       session[:user_id] = authorized_user.id
-      flash[:notice] = "Wow Welcome again"
       redirect_to users_path
     else
       flash[:notice] = "Invalid Username or Password"
       flash[:color]= "invalid"
       render "login"
+    end
+  end
+
+  def index
+    if params[:args] != ""
+      @users = User.where(nickname: params[:args])
+      @hikes = Hike.where(name: params[:args])
+    else
+      @users = User.all
+      @hikes = Hike.all
     end
   end
 
@@ -31,4 +40,5 @@ class SessionsController < ApplicationController
     session[:user_id] = nil
     redirect_to :action => 'login'
   end
+
 end
