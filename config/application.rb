9c3616fa6c200d1d69bf017b46523265436fb689
polyc <ichigo69600@gmail.com>
2018-10-03 18:01:59 +0200

    necessary files for rspec and factorybot

--- config/application.rb
+++ config/application.rb
@@ -27,5 +27,16 @@ module Hiker
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.generators do |g|
+      g.test_framework :rspec,
+        :fixtures => true,
+        :view_specs => false,
+        :helper_specs => false,
+        :routing_specs => false,
+        :controller_specs => true,
+        :request_specs => true
+      g.fixture_replacement :factory_bot, :dir => "spec/factories"
+end
   end
 end
