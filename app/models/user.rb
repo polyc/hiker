d00048fa544ab9167469992075b91b31a30b02b9
polyc <ichigo69600@gmail.com>
2018-10-04 19:37:02 +0200

    remove unused method

--- app/models/user.rb
+++ app/models/user.rb
@@ -90,13 +90,6 @@ class User < ActiveRecord::Base
 
   #third party auth methods
   #########################
-  def self.new_with_session(params, session)
-    super.tap do |user|
-      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
-        user.email = data["email"] if user.email.blank?
-      end
-    end
-  end
 
   def self.from_omniauth(auth)
    where(provider: auth.provider, uid: auth.uid).first_or_create! do |user|
